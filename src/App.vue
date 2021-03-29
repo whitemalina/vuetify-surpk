65721228cf93c05f3638bda5e7cf258ea509e5b3
WhiteMalina <danil.bayramov.03@gmail.com>
2021-03-30 02:07:22 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,109 @@
+<template>
+  <div id="app">
+    <v-app id="inspire">
+      <v-app id="inspire">
+        <v-navigation-drawer
+          v-model="drawer"
+          app
+        >
+          <v-list dense>
+            <v-list-item link>
+              <v-list-item-action>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>Создание заявки</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-action>
+                <v-icon>mdi-email</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>Обратная связь</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar
+          app
+          color="#23539c"
+          flat="true"
+          collapse-on-scroll="true"
+          dark
+        >
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-toolbar-title>СПК ПОДДЕРЖКА</v-toolbar-title>
+          <ModalSend></ModalSend>
+        </v-app-bar>
+
+        <v-main>
+          <v-container
+            class="fill-height"
+            fluid
+          >
+            <v-row
+              align="center"
+              justify="center"
+            >
+              <v-col class="text-center">
+                <v-tooltip left>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      :href="source"
+                      icon
+                      large
+                      target="_blank"
+                      v-on="on"
+                    >
+                      <v-icon large>mdi-code-tags</v-icon>
+
+                    </v-btn>
+                  </template>
+                  <span>Source</span>
+                </v-tooltip>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-main>
+        <v-footer
+          color="#23539c"
+          app
+        >
+          <span class="white--text copyc">&copy; {{ new Date().getFullYear() }} <a href="https://www.surpk.ru">www.surpk.ru</a></span>
+        </v-footer>
+      </v-app>
+    </v-app>
+  </div>
+
+</template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld";
+import ModalSend from "./components/ModalSend";
+export default {
+  name: "App",
+  components: {
+    //HelloWorld,
+    ModalSend,
+  },
+  props: {
+    source: String,
+
+  },
+  data: () => ({
+    firstname: '',
+    lastname: '',
+    drawer: true,
+    dialog: false,
+    items: ['СП-1', 'СП-2', 'СП-3', 'СП-4'],
+  }),
+};
+</script>
+
+<style lang="css">
+.copyc a{
+  color: white !important;
+}
+</style>
---
+++ src/components/ModalSend.vue
@@ -0,0 +1,117 @@
+<template v-slot:extension>
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        fab
+        color="red"
+        bottom
+        left
+        absolute
+        v-on="on"
+        v-bind="attrs"
+        @click="dialog = !dialog"
+      >
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline">Создание заявки</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12" sm="6" md="4">
+              <v-text-field
+                v-model="firstname"
+                label="Имя*"
+                :rules="nameRules"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="4">
+              <v-text-field
+                v-model="lastname"
+                :rules="lastNameRules"
+                label="Фамилия*"
+                hint=""
+                required
+              ></v-text-field>
+            </v-col>
+            <!--                      <v-col cols="12" sm="6" md="4">-->
+            <!--                        <v-text-field-->
+            <!--                          label="Legal last name*"-->
+            <!--                          hint="example of persistent helper text"-->
+            <!--                          persistent-hint-->
+            <!--                          required-->
+            <!--                        ></v-text-field>-->
+            <!--                      </v-col>-->
+            <v-col cols="12">
+
+            </v-col>
+            <v-col cols="12">
+              <!--                        <v-text-field label="Password*" type="password" required></v-text-field>-->
+              <v-textarea
+                label="Проблема"
+                :rules="[v => !!v || 'Поле обязательно']"
+                counter
+              ></v-textarea>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-select
+                :items="['СП-1', 'СП-2', 'СП-3', 'СП-4', 'СП-5']"
+                label="СП"
+                :rules="[v => !!v || 'Поле обязательно']"
+                required
+              ></v-select>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field
+                v-model="firstname"
+                label="Кабинет*"
+                :rules="nameRules"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+        <small>*Обязательные поля</small>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="dialog = false">Закрыть</v-btn>
+        <v-btn color="blue darken-1" text @click="validate">Отправить</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+
+  },
+  props: {
+    source: String,
+  },
+  data: () => ({
+    firstname: '',
+    lastname: '',
+    drawer: true,
+    dialog: false,
+    items: ['СП-1', 'СП-2', 'СП-3', 'СП-4'],
+    nameRules: [
+      v => !!v || 'Имя обязательно',
+    ],
+    lastNameRules: [
+      v => !!v || 'Фамилия обязательна',
+    ],
+  }),
+  methods: {
+    validate() {
+      this.$refs.form.validate()
+    },
+  }
+};
+</script>
